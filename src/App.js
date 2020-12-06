6748ce2c7d1cb64a7b4ae0218d1255d015c064c2
NickEleftheriadis <nikele6002@gmail.com>
2020-12-06 18:50:20 +0200

    design change

--- src/App.js
+++ src/App.js
@@ -17,11 +17,13 @@ class App extends Component {
     
     
     return (  
-      <div >
+      <div  >
       <Container>
-        <Row>
-          <Col><h1 className="onoma-style">Petrakis Counter</h1></Col>
-          <Col><button className="button-plus-style" onClick={this.Increasment} >+</button></Col>
+        <Row className="App">
+          <Col ><h1 className="onoma-style">Petrakis Counter</h1></Col>
+        </Row>
+        <Row className="App">
+          <Col ><button className="button-plus-style" onClick={this.Increasment} >+</button></Col>
           <Col><span  >{this.state.count}</span></Col>
           <Col><button className="button-minus-style" onClick={this.Decreasment} >-</button></Col>
         </Row>
